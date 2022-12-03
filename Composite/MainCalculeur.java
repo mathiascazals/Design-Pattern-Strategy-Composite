43e63b63580917f4f59152a3b8241c4a53467eed
Andy Gonzales <91250741+GonzalesAndy@users.noreply.github.com>
2022-12-03 20:59:14 +0100

    Changement de la façon d'ajouter

--- Composite/MainCalculeur.java
+++ Composite/MainCalculeur.java
@@ -5,26 +5,21 @@ public class MainCalculeur {
 
 	public static void main(String[] args) {
 		
-		Moteur deuxjz = new Moteur(1000);
+		Composant deuxjz = new Moteur(1000);
 		System.out.println("Le prix de mon moteur est: "+deuxjz.getPrice()+" €");
 		
-		Roue roueavg = new Roue(100);
-		Roue roueavd = new Roue(100);
-		Roue rouearg = new Roue(100);
-		Roue roueard = new Roue(100);
+		Composant roueavg = new Roue(100);
+		Composant roueavd = new Roue(100);
+		Composant rouearg = new Roue(100);
+		Composant roueard = new Roue(100);
 		System.out.println("Le prix d'une roue est: "+roueavd.getPrice()+" €");
 		
-		Carrosserie carrosserie = new Carrosserie(700);
+		Composant carrosserie = new Carrosserie(700);
 		System.out.println("Le prix de la carrosserie est: "+carrosserie.getPrice()+" €");
 
 		
 		VoitureComposite supra = new VoitureComposite();
-		supra.add(deuxjz);
-		supra.add(roueavd);
-		supra.add(roueavg);
-		supra.add(roueard);
-		supra.add(rouearg);
-		supra.add(carrosserie);
+		supra.add(deuxjz,roueavd,roueavg,roueard,rouearg,carrosserie);
 		System.out.print("Le prix de ma voiture est: "+supra.getPrice()+" €.");
 		}
 
